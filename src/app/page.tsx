d0b6a816396c83df881b1326cc32bb513b9dadab
gil7788 <gil7788@gmail.com>
2025-06-21 08:47:35 +0300

    Update Logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/auth-zk.png"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -29,13 +29,13 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{' '}
+            Get started by {' '}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
+              Authenticating
             </code>
             .
           </li>
-          <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
+          <li className="tracking-[-.01em]">Link your digital identities and prove ownership without compramising your privacy.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
